e6930f3e888cafba98996fa906ace20b89271da0
baptiste66 <101480985+baptiste66@users.noreply.github.com>
2023-03-14 14:05:15 +0100

    bug page erreur

--- src/composants/card/card.js
+++ src/composants/card/card.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
 import Data from '../../datas/data'
+import { useNavigate } from 'react-router-dom';
 
-export default function Card (){
-    return(
-        <section className='card'>
-            {Data.map((card) => (
-                <NavLink to={`/logement/${card.id}`} className='card__item' key={card.id}>
-                    <img className='card__img' src={card.cover} alt='annonce de logement en location' />
-                    <h2 className='card__title'>{card.title}</h2>
-                </NavLink>
-            ))}
-        </section>
-    );
+
+
+export default function Card (){ const navigate = useNavigate();
+
+  const handleCardClick = (id) => {
+    // Vérifier si l'id correspond à un logement dans la liste de données
+    const logement = Data.find((logement) => logement.id === id);
+    if (logement) {
+      navigate(`/logement/${id}`);
+    } else {
+      navigate('/error');
+    }
+  };
+  return (
+    <section className='card'>
+      {Data.map((card) => (
+        <div className='card__item' key={card.id} onClick={() => handleCardClick(card.id)}>
+          <img className='card__img' src={card.cover} alt='annonce de logement en location' />
+          <h2 className='card__title'>{card.title}</h2>
+        </div>
+      ))}
+    </section>
+  );
 }
--- src/pages/logement.js
+++ src/pages/logement.js
@@ -10,50 +10,57 @@ import Star from '../composants/star/star'
 import '../style/pages/logement.css'
 import'../style/composants/caroussel.css'
 
-
-export default function Logement(){ 
-    const {id} = useParams();
-    const infoProduct = Data.find((a) => a.id === id);
-    const {title, location, tags, host, rating, description, equipments} = infoProduct
-// boucle pour tag pour les séparer
-    return(infoProduct ?
-        <div>
-        <Header/>
+export default function Logement() {
+    const { id } = useParams();
+    const infoProduct = Data.find((a) => a.id === String(id));
+    return infoProduct ? (
+      <div>
+        <Header />
         <main>
-        <Caroussel/>
-        <section className='info__product'>
-            <div className='direction'>
-            <div className='left'>
-            <div className='location'>
-           <h1>{title}</h1> 
-           <h2>{location}</h2>
-           </div>
-           <div className='tag'>
-            {tags.map((tag) => (
-                <p key={tag}>{tag}</p>
-            ))}
+          <Caroussel />
+          <section className="info__product">
+            <div className="direction">
+              <div className="left">
+                <div className="location">
+                  <h1>{infoProduct.title}</h1>
+                  <h2>{infoProduct.location}</h2>
+                </div>
+                <div className="tag">
+                  {infoProduct.tags.map((tag) => (
+                    <p key={tag}>{tag}</p>
+                  ))}
+                </div>
+              </div>
+              <div className="right">
+                <span className="inline">
+                  <p>{infoProduct.host.name}</p>
+                  <img
+                    src={infoProduct.host.picture}
+                    alt="loueur du logement sélectionner"
+                  ></img>
+                </span>
+                <Star star={infoProduct.rating} />
+              </div>
             </div>
+            <div className="accordion-log">
+              <Accordion title="Description" content={infoProduct.description} />
+              <Accordion
+                title="Equipements"
+                content={
+                  <ul>
+                    {infoProduct.equipments.map((equipment) => (
+                      <li key={equipment}>{equipment}</li>
+                    ))}
+                  </ul>
+                }
+              />
             </div>
-           <div className='right'>
-            <span className='inline'><p>{host.name}</p>
-            <img src={host.picture} alt='loueur du logement sélectionner'></img></span>
-            <Star star={rating}/>
-           </div>
-            </div>
-           <div className='accordion-log'>
-            <Accordion title='Description' content={description} />
-            <Accordion title='Equipements' content={
-                            <ul>
-                                {equipments.map((equipment) => (
-                                    <li key={equipment}>{equipment}</li>
-                                ))}
-                            </ul>
-                        } />  
-            
-           </div>
-        </section>
-        </main> 
-        <Footer/>
-        </div>:<Error/>
-    )
-}
+          </section>
+        </main>
+        <Footer />
+      </div>
+    ) : (
+      <Error />
+    );
+  }
+
