506402ade41b2cef02f76f1f786b8262fe691a70
baptiste66 <101480985+baptiste66@users.noreply.github.com>
2023-03-14 17:44:09 +0100

    renvoie vers /home

--- src/router/router.js
+++ src/router/router.js
@@ -1,18 +1,18 @@
 import React from 'react'; 
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route,Navigate} from 'react-router-dom';
 import Home from '../pages/home'; 
 import Error from '../pages/404';
 import About from '../pages/about';
 import  Logement from '../pages/logement';
 import '../style/pages/home.css';
 
-// deux fois home pour le mettre en page de démarrage et pour renvoyer page err quand change l'url
+// deux fois home pour le mettre en page de démarrage et renvoyer vers /home et pour renvoyer page err quand change l'url
 function Routing(){
     return(
         <div>
            
             <Routes>
-                <Route path="/" element={<Home />} />
+            <Route path='/' element={<Navigate to="/home" />} />
                 <Route path='/home' element={<Home />} />
                 <Route path='*' element={<Error />} />
                 <Route path='/about' element={<About />} />
